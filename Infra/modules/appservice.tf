4c5ec184bfc8e187956390c529260fa47e243046
Adinath Dukare <Adinath.Dukare@allscripts.com>
2021-10-28 14:55:29 +0530

    Added infra folder

--- Infra/modules/appservice.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_app_service" "example" {
-#   name                = "example-app-service"
-  for_each              = var.apps
-  name                  = format("app-%s-%s",each.value.app_name,each.value.version)
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id = var.plan_id
-}
