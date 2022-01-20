d588ef25e5fdc3a73e34a460d80a263071aae52e
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2022-01-20 13:46:13 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
-  param (
-    [parameter(Mandatory=$True)]
-    [string]$test_name
-	)
+#   param (
+#     [parameter(Mandatory=$True)]
+#     [string]$test_name
+# 	)
 	
 	write-host "welcome"
-	write-host $test_name
+# 	write-host $test_name
